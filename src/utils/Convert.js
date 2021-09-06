5a1cdca70af9d02ea3c4641b3b7c44cdd13a4a21
Desktop <spyatorio@gmail.com>
2021-09-06 11:07:08 -0400

    Implementacion de la clase convert

---
+++ src/utils/Convert.js
@@ -0,0 +1,17 @@
+//Laboratori Nro 2.
+//desarrollar esta dos funciones.
+//
+class Convert {
+  constructor() {}
+
+  //recibe una parametro de tipo arreglo
+  // que es la lista enlazada
+  //regresa una matriz de adyacencia
+  convertToMatrix(graph) {}
+  //recibe una matriz de adyacencia
+  //Y regresa una lista enlazada
+  //regresa un arreglo de tipo graph
+  //[v1, v2, v3, v4, v5]
+  convertToList(matrix) {}
+}
+export default Convert;
